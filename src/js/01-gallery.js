6d7749631eb4fced4ae2809076d904b608bb85a5
xrsQugi <02.04.5.art@gmail.com>
2023-10-11 23:24:06 +0300

    video player

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    (acc += `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`),
+  ""
+);
+
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+
+// use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+  });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const current_time = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(current_time, seconds);
+}
+function setCurrentTime() {
+  if (!localStorage.getItem(current_time)) return;
+  player.setCurrentTime(localStorage.getItem(current_time));
+}
+
+setCurrentTime();
+
+
+player
+  .setColors(['#000000', '#ffffff'])
+  .then(function (color) {
+    console.log('--- Color values updated ---');
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the color');
+  });
